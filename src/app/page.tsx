a690100a4082c3768b9afeb09a0edd5187311f9c
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-05-16 16:30:10 +0200

    u

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,6 @@
-"use client";
 import { Section } from "@/exports";
 import { navbar } from "@/constants";
-import { useEffect } from "react";
-import Lenis from "lenis";
 export default function Home() {
-  useEffect(() => {
-    const lenis = new Lenis();
-    function raf(time: number) {
-      lenis.raf(time);
-
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
-  }, []);
-
   return (
     <main>
       {navbar.map((item) => (
