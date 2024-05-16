c7cbbd9b32bb85ca8d6e30a14c3533f792103f0f
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-05-16 14:32:11 +0200

    Update Hamburger.tsx

--- src/components/layout/Hamburger.tsx
+++ src/components/layout/Hamburger.tsx
@@ -2,15 +2,15 @@
 import { motion } from "framer-motion";
 
 export default function Hamburger({ toggle }: { toggle?: boolean }) {
-  const span_1 = toggle ? { rotate: 45, translateY: 4 } : {};
-  const span_2 = toggle ? { rotate: -45, translateY: -4 } : {};
+  const span_1 = toggle ? { transalteY: 8, rotate: 45 } : {};
+  const span_2 = toggle ? { translateY: -8, rotate: -45 } : {};
   return (
     <>
       {Array.from({ length: 2 }, (_, index) => index).map((index) => (
         <motion.span
           key={index}
-          className="bg-stone-900 w-6 h-0.5 rounded-full"
           animate={index === 0 ? span_1 : span_2}
+          className="h-0.5 w-6 bg-stone-900 rounded-full"
           transition={{ duration: 0.5, type: "tween", ease: "easeInOut" }}
         />
       ))}
