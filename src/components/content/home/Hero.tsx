d069afd2f11b8dcf6c8ce8ffea21cf16d5cd414f
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-05-22 14:04:56 +0200

    Update Hero.tsx

--- src/components/content/home/Hero.tsx
+++ src/components/content/home/Hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Variants, motion } from "framer-motion";
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 const Spline = dynamic(() => import("@splinetool/react-spline"), {
   ssr: false,
@@ -65,13 +66,15 @@ const HeroP = ({ children, className }: Text) => {
 
 const SplineCubes = () => {
   return (
-    <motion.div
-      initial={{ scale: 0, opacity: 0 }}
-      animate={{ scale: 1, opacity: 1 }}
-      transition={{ duration: 3, delay: 5 }}
-    >
-      <Spline scene="/models/scene.splinecode" />
-    </motion.div>
+    <Suspense fallback={<p>3D CUBES</p>}>
+      <motion.div
+        initial={{ scale: 0, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ duration: 3, delay: 5 }}
+      >
+        <Spline scene="/models/scene.splinecode" />
+      </motion.div>
+    </Suspense>
   );
 };
 
